b5681fff3d4e71ac220cc1f9f969359a2a5813ea
Vincenzo D'Aniello <vidaniello@gmail.com>
2023-05-10 17:28:14 +0200

    develop progress

---
+++ src/test/java/com/amazon/Tests.java
@@ -0,0 +1,48 @@
+package com.amazon;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+import com.amazon.SellingPartnerAPIAA.AWSAuthenticationCredentials;
+import com.amazon.api.SellersApi;
+
+public class Tests {
+	
+	static {
+		// Log4j from 2.17.>
+		//System.setProperty("log4j2.Configuration.allowedProtocols", "http");
+
+		// URL file di configurazione Log4j2
+		System.setProperty("log4j.configurationFile", "https://gist.github.com/vidaniello/c20e29cdffb407ec5d3c773fb92786b9/raw/92c8e809f51133ef56e4867a6cabb0744ee6b9b6/log4j2.xml");
+
+		// Tips per java.util.logging
+		System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
+
+		// private org.apache.logging.log4j.Logger log =
+		// org.apache.logging.log4j.LogManager.getLogger();
+	}
+	
+	private Logger log = LogManager.getLogger();
+	
+	@Test
+	public void test1() {
+		try {
+			
+			//AWSAuthenticationCredentials aWSAuthenticationCredentials = AWSAuthenticationCredentials
+			
+			
+			SellersApi sellersApi = new SellersApi.Builder()
+	                  .awsAuthenticationCredentials(null)
+	                  .lwaAuthorizationCredentials(null)
+	                  .awsAuthenticationCredentialsProvider(null)
+	                  .endpoint("https://sellingpartnerapi-na.amazon.com") // use Sandbox URL here if you would like to test your applications without affecting production data.
+	                  .build();
+			
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+			throw new AssertionError(e);
+		}
+	}
+
+}
